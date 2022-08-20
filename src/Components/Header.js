ae0e3c5c1594d8738096d942ff069eed3b1a867f
Vika Zaichuk <viktoriia.zaichuk@gmail.com>
2022-08-20 23:50:40 +0200

    Theme mode switch - added switch option to the toggle button.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,24 @@
 import React from "react";
+import useDarkMode from "use-dark-mode";
+import { useTheme } from "./Theme.js";
 
-import darkmode from './../images/icon-sun.svg';
+import darkmodes from './../images/icon-sun.svg';
+import lightmodes from './../images/icon-moon.svg';
 
-function Header() {
-    return (
+const Header = () => {
+  const darkMode = useDarkMode();
+  const theme = useTheme();
+
+  return (
       <div className='Header'>
         <h1>TODO</h1>
         <div>
-            <img src={darkmode} alt='Cange design mode' />
+          <button className="SwitchTheme-btn" type="button" onClick={darkMode.toggle}>
+            {theme === "dark-mode" ? <img src={darkmodes} alt='Change design mode' /> : <img src={lightmodes} alt='Change design mode' />}
+          </button>
         </div>
       </div>
     );
   }
   
-  export default Header;
+export default Header;
